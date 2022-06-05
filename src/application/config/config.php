7b5e9037ce1dc24aa868048d7b40c88e0443075a
bspbey <98183612+bspbey@users.noreply.github.com>
2022-06-05 19:19:05 +0300

    Update config.php

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'buraya kurulu olduğu dizin';
+$config['base_url'] = 'http://demirciitiraf.unaux.com/';
 
 /*
 |--------------------------------------------------------------------------
